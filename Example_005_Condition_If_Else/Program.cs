2ca541ffeabd32702da8ef9c5744992ed4265693
Serg_Malov <serge-malov@yandex.ru>
2023-07-30 18:36:21 +0300

    Добавлена новая консоль Conditional_If_Else

---
+++ Example_005_Condition_If_Else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша") {
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else{
+    Console.WriteLine("Привет, " + userName);
+}
