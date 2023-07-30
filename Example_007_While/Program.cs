190be5293d1d311db228b1eb1cfdc6a6bcf3b802
Serg_Malov <serge-malov@yandex.ru>
2023-07-30 19:29:55 +0300

    Добавлена новая консоль While

---
+++ Example_007_While/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+int xA = 40, yA = 1,
+    xB = 1, yB = 30,
+    xC = 80, yC = 30;
+
+int x = xA, y = yA;
+int count = 0;
+
+Console.SetCursorPosition(xA, yA);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xB, yB);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xC, yC);
+Console.WriteLine("+");
+
+while (count < 10000) {
+    int rand = new Random().Next(0, 3); // [0, 3)
+
+    if (rand == 0) {
+        x = (x + xA)/2;
+        y = (y + yA)/2;
+    }
+
+    if (rand == 1) {
+        x = (x + xB)/2;
+        y = (y + yB)/2;
+    }
+
+    if (rand == 2) {
+        x = (x + xC)/2;
+        y = (y + yC)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
