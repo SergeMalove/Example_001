f8212b791e8e050edbfd89317e9b0df44826c514
Serg_Malov <serge-malov@yandex.ru>
2023-08-02 21:26:19 +0300

    Оптимизированное решение с циклом for

--- Example_010_MethodArray/Program.cs
+++ Example_010_MethodArray/Program.cs
@@ -1,14 +1,10 @@
 ﻿int[] numbers = {23, 44, 28, 36, 12, 47, 18, 31, 15};
 
-int n = numbers.Length;
 int find = 36;
 
-int index = 0;
-
-while (index < n) {
-    if (numbers[index] == find) {
-        Console.WriteLine(index);
+for (int i = 0; i < numbers.Length; i++) {
+    if (numbers[i] == find) {
+        Console.WriteLine(i);
         break;
     }
-    index++;
 }
