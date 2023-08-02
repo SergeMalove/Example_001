15f0eb4ad25408d87d1740519eeedbc8a407b651
Serg_Malov <serge-malov@yandex.ru>
2023-08-02 21:20:46 +0300

    Решение, как в лекции

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = {23, 44, 28, 36, 12, 47, 18, 31, 15};
+
+int n = numbers.Length;
+int find = 36;
+
+int index = 0;
+
+while (index < n) {
+    if (numbers[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
