14138ba70a24b3087ea84f129aca9099977fd765
Serg <serge-malov@yandex.ru>
2023-08-03 09:07:26 +0300

    Методы заполнения и вывода массива на экран

---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] array) {
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array) {
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(array[index]);
+        index++;
+    }
+}
+
+int[] numbers = new int[10];
+
+FillArray(numbers);
+PrintArray(numbers);
