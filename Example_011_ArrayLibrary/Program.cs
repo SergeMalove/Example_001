aab1967fdd69ec0fa8d6a250fc33376d984a6f22
Serg <serge-malov@yandex.ru>
2023-08-03 09:22:18 +0300

    Добавлено решение с поиском индекса элемента

--- Example_011_ArrayLibrary/Program.cs
+++ Example_011_ArrayLibrary/Program.cs
@@ -18,7 +18,26 @@ void PrintArray(int[] array) {
     }
 }
 
+int IndexOf(int[] array, int find) {
+    int length = array.Length;
+    int index = 0;
+    int result = -1;
+
+    while (index < length) {
+        if (array[index] == find) {
+            result = index;
+        }
+        index++;
+    }
+    return result;
+}
+
 int[] numbers = new int[10];
 
 FillArray(numbers);
 PrintArray(numbers);
+Console.WriteLine();
+
+int position = IndexOf(numbers, 4);
+
+Console.WriteLine(position);
